b559fd41174e2b215e179db11e3416dfc7bb0d5f
Basman1974 <proalven@gmail.com>
2026-09-25 22:53:27 +0200

    Corregir caché offline de archivos versionados

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,15 @@
-const CACHE='calculadora-construccion-v1';
+const CACHE='calculadora-construccion-20260925-controls-audit2';
 const CORE=[
   './','index.html','style.css','data.js','app.js','app-a1.js','app-a2.js','app-b.js',
-  'ui-a.html','ui-b.html','ui-c.html','logo.png'
+  'ui-a.html','ui-b.html','ui-c.html','manifest.json','logo.png'
 ];
 self.addEventListener('install',event=>{
-  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)).then(()=>self.skipWaiting()));
+  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)).catch(()=>{}));
+  self.skipWaiting();
 });
 self.addEventListener('activate',event=>{
-  event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));
+  event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
+  self.clients.claim();
 });
 self.addEventListener('fetch',event=>{
   if(event.request.method!=='GET') return;
@@ -16,8 +18,8 @@ self.addEventListener('fetch',event=>{
   event.respondWith(
     fetch(event.request).then(res=>{
       const copy=res.clone();
-      caches.open(CACHE).then(cache=>cache.put(event.request,copy));
+      caches.open(CACHE).then(cache=>cache.put(event.request,copy)).catch(()=>{});
       return res;
-    }).catch(()=>caches.match(event.request).then(r=>r||caches.match('./')))
+    }).catch(()=>caches.match(event.request,{ignoreSearch:true}).then(r=>r||caches.match('./')))
   );
 });
